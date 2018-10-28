d6912e8d56ebff8444288ca44f1e4ca3f159deed
Kesara Wimal <kesarawimal9@gmail.com>
2018-10-28 21:33:00 +0530

    search result page

---
+++ src/app/components/search-results/search-results.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+import {WeatherService} from '../../services/weather.service';
+
+@Component({
+  selector: 'app-search-results',
+  templateUrl: './search-results.component.html',
+  styleUrls: ['./search-results.component.css']
+})
+export class SearchResultsComponent implements OnInit {
+  keyword: string;
+  data: any;
+  constructor(private route: ActivatedRoute, private weatherService: WeatherService) { }
+
+  ngOnInit() {
+    this.keyword = this.route.snapshot.paramMap.get('keyword');
+    this.weatherService.search(this.keyword).subscribe( (value => {
+      console.log(value);
+      this.data = value;
+    }));
+  }
+
+}
