21d446688d9f224f09d1a12f46828fc65d297e16
Kesara Wimal <kesarawimal9@gmail.com>
2018-10-28 19:47:35 +0530

    angular routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {HomeComponent} from './components/home/home.component';
+import {WoeidComponent} from './components/woeid/woeid.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'weather/:woeid',
+    component: WoeidComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,25 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { WeatherService } from './services/weather.service';
+import { WeatherComponent } from './components/weather/weather.component';
+import { HomeComponent } from './components/home/home.component';
+import {HttpClientModule} from '@angular/common/http';
+import { WoeidComponent } from './components/woeid/woeid.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WeatherComponent,
+    HomeComponent,
+    WoeidComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
   ],
-  providers: [],
+  providers: [WeatherService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
